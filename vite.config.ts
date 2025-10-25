6c1507c871db2958a0000d8fc4c3a51c3f60f35c
xtt28 <155261788+xtt28@users.noreply.github.com>
2025-10-25 07:47:05 -0400

    Add PWA resources.

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,34 @@
 import legacy from '@vitejs/plugin-legacy'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    legacy()
+    legacy(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico'],
+      manifest: {
+        name: 'myBCA',
+        short_name: 'myBCA',
+        description: 'The all-in-one app for BCA students',
+        icons: [
+          {
+            src: 'pwa/icon-pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa/icon-pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          }
+        ]
+      }
+    })
   ],
   test: {
     globals: true,
