dd890fab9faefb5dd5f2d88645142b279cfa643d
xtt28 <155261788+xtt28@users.noreply.github.com>
2025-12-13 10:43:05 -0500

    Add event form.

--- src/components/events/EventsList.tsx
+++ src/components/events/EventsList.tsx
@@ -1,4 +1,4 @@
-import { IonList, IonText } from "@ionic/react";
+import { IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonList, IonText } from "@ionic/react";
 import { EventsListEntry } from "./EventsListEntry";
 import { Event } from "../../network/pocketbase/pocketbase";
 
@@ -9,6 +9,12 @@ type EventsListProps = {
 export const EventsList: React.FC<EventsListProps> = ({ data }) => {
   return (
     <>
+      <div className="ion-padding">
+        <IonText>
+          Want your club's events to be featured here? Fill out &#32;
+          <a href={import.meta.env.VITE_EVENT_FORM_URL} target="_blank" rel="noopener noreferrer">this form</a>.
+        </IonText>
+      </div>
       {data.length === 0 ? (
         <div className="ion-text-center ion-padding">
           <IonText>There are no upcoming events.</IonText>
