9715692192173c2ccd616743a75a8847e2258ba8
xtt28 <155261788+xtt28@users.noreply.github.com>
2026-01-16 08:43:51 -0500

    Add modal for lunch menu items.

--- src/components/lunch/LunchList.tsx
+++ src/components/lunch/LunchList.tsx
@@ -1,5 +1,6 @@
-import { IonItem, IonItemDivider, IonLabel, IonThumbnail } from "@ionic/react";
+import { IonButton, IonContent, IonHeader, IonItem, IonItemDivider, IonLabel, IonModal, IonText, IonThumbnail, IonTitle, IonToolbar } from "@ionic/react";
 import { components } from "../../network/openapi/v1";
+import { useState } from "react";
 
 type LunchListProps = {
   data: components["schemas"]["MenuDayDto2"];
@@ -9,6 +10,19 @@ type LunchListProps = {
 const UNWANTED_SECTIONS = [3676, 3088, 3090];
 
 export const LunchList: React.FC<LunchListProps> = ({ data }) => {
+  const [selectedItem, setSelectedItem] = useState<components["schemas"]["FoodItemDto"] | null>(null);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openModal = (item: components["schemas"]["FoodItemDto"]) => {
+    setSelectedItem(item);
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+    setSelectedItem(null);
+  };
+
   return (
     <>
       {data &&
@@ -37,6 +51,8 @@ export const LunchList: React.FC<LunchListProps> = ({ data }) => {
                   return (
                     <IonItem
                       key={index}
+                      button
+                      onClick={() => openModal(item.food)}
                     >
                       {item.food.imageUrl && (
                         <IonThumbnail slot="start">
@@ -55,6 +71,34 @@ export const LunchList: React.FC<LunchListProps> = ({ data }) => {
               })}
           </>
         ))}
+      <IonModal
+        isOpen={isOpen}
+        onDidDismiss={closeModal}
+        initialBreakpoint={0.75}
+        breakpoints={[0, 0.75]}
+        expandToScroll={false}
+      >
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>{selectedItem?.name}</IonTitle>
+            <IonButton onClick={closeModal} slot="end" fill="clear">
+              Close
+            </IonButton>
+          </IonToolbar>
+        </IonHeader>
+
+        <IonContent className="ion-padding">
+          {selectedItem?.imageUrl && <img src={selectedItem.imageUrl} alt={selectedItem.name ?? ""} />}
+          {selectedItem?.description && (
+            <>
+              <IonText>
+                <h3>Description</h3>
+              </IonText>
+              <p>{selectedItem?.description}</p>
+            </>
+          )}
+        </IonContent>
+      </IonModal>
     </>
   );
 };
